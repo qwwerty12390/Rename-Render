6a1160f60d7c98fbb9f2ec1d8309501b0d3cacba
qwwerty12390 <139214618+qwwerty12390@users.noreply.github.com>
2024-01-26 21:42:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "27808193")
+API_ID = os.environ.get("API_ID", "23681812")
 
-API_HASH = os.environ.get("API_HASH", "c9964aa48c0fde0a3e7fee755a1b8a4e")
+API_HASH = os.environ.get("API_HASH", "3152d0cda0a21787db0c924f522374b7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6706677415:AAG4ZhkJVghanh_yv6bGmltJoQOXuoL_2Qo") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6858377496:AAEHVaWfhp-2LC11YEm4oSGes5Fw7iiNJJI") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "KOREAN_DRAMA_FILES_HINDI") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "Dramafilez") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "cluster0")     
 
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://bhankharvansh:8xS4igRIb41EiJnz@cluster0.ctbwmpb.mongodb.net/?retryWrites=true&w=majority")
  
@@ -27,7 +27,7 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '999739828').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6710996831').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
